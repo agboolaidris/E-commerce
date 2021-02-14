40bc0c7b32ab1a60f3bf81b3ca519a804744c51f
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-14 14:54:55 +0100

    update files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+
+//setting up environment variable
+require("dotenv").config();
+
+//setting up mongodb
+mongose.connect(process.env.MONGODB_SECRET, { useNewUrlParser: true }, () => {
+  console.log("database connected successful");
+});
+
+//setting up bodyparser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
