7bbd10464853010fa65c7ca3ecb037414c7ecb26
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-03 15:46:50 +0100

    update

--- admin/src/Component/Layout/Nav/Guest.jsx
+++ admin/src/Component/Layout/Nav/Guest.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Guest() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home </Link>
+    <>
+      <li className="text-2lg sm:mx-4">
+        <Link to="/" className="text-white hover:text-pink-600">
+          Home{" "}
+        </Link>
       </li>
-    </ul>
+      <li className="text-2lg bg-pink-400 px-3 py-1 sm:mx-4 hover:bg-pink-200">
+        <Link to="/login" className="text-gray-800 hover:text-pink-600">
+          Login
+        </Link>
+      </li>
+      <li className="text-2lg sm:mx-4 border-pink-400  bg-gray-200 px-3 py-1">
+        <Link to="/" className="text-pink hover:text-pink-600">
+          Register
+        </Link>
+      </li>
+    </>
   );
 }
 
--- admin/src/Component/Layout/Nav/Index.jsx
+++ admin/src/Component/Layout/Nav/Index.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Guest from "./Guest";
 
 function Nav() {
+  const [state, setstate] = useState(false);
+
+  const handleClick = () => {
+    if (state) {
+      setstate(false);
+    } else {
+      setstate(true);
+    }
+  };
   return (
-    <header className="bg-gray-800">
-      <nav className="container mx-auto">
-        <span className="logo">IRIS SHOP</span>
-        <div className="harmburger"></div>
-        <>{<Guest />}</>
+    <header className="bg-gray-800 sticky top-0 ">
+      <nav className="container mx-auto flex justify-between py-3 items-center px-2 ">
+        <div className="harmburger sm:hidden z-10" onClick={handleClick}>
+          hello
+        </div>
+        <span className="logo text-white text-2xl">IRIS SHOP</span>
+        <ul
+          className={
+            state
+              ? "nav-item menu bg-gray-800"
+              : "nav-item  sm:bg-transparent flex justify-center items-center"
+          }
+        >
+          {<Guest />}
+        </ul>
       </nav>
     </header>
   );
