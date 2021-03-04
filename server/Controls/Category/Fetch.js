cd24b6b49d6660192b4a5e9b743c22dbdddf0019
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-04 16:33:30 +0100

    update files

--- server/Controls/Category/Fetch.js
+++ server/Controls/Category/Fetch.js
@@ -1,10 +1,30 @@
 const Category = require("../../Models/Category");
 
+const category = (data, parentId = null) => {
+  let categorylist = [];
+  let list;
+  if (!parentId) {
+    list = data;
+  } else {
+    list = data.filter((e) => e.parentId == parentId);
+  }
+  for (let cat of list) {
+    categorylist.push({
+      _id: cat._id,
+      name: cat.name,
+      slug: cat.slug,
+      children: category(data, cat._id),
+    });
+  }
+  return categorylist;
+};
+
 const fetch = async (req, res) => {
   try {
-    const response = await Category.find();
-    if (!response)
+    const data = await Category.find();
+    if (!data)
       return res.status(401).json({ msg: "nothing found in database" });
+    const response = category(data);
     res.json(response);
   } catch (error) {
     res.status(400).json({ msg: error.message });
