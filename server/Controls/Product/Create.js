66b22f1bb3131cc1a3f340426088405766189c64
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-05 12:39:49 +0100

    update

--- server/Controls/Product/Create.js
+++ server/Controls/Product/Create.js
@@ -1,7 +1,10 @@
 const User = require("../../Models/Product");
+const path = require("path");
 const Create = async (req, res) => {
   try {
-    res.json({ msg: "create" });
+    console.log(path.dirname(__dirname));
+
+    res.json({ file: req.file, body: req.body });
   } catch (error) {
     res.status(400).json({ msg: error.message });
   }
--- server/Routes/Product.js
+++ server/Routes/Product.js
@@ -1,7 +1,18 @@
 const Route = require("express").Router();
 const Authorize = require("../Middleware/authorize");
 const Multer = require("multer");
-var upload = Multer({ dest: "uploads/" });
+const shortid = require("shortid");
+const path = require("path");
+
+const storage = Multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + "-" + file.orginalname);
+  },
+});
+const upload = Multer({ storage });
 
 Route.post(
   "/",
